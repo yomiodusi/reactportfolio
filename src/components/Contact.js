10bcc2ac44bb2ad0859ad1b8b8f19fb89d6f5799
yomi odusi <yomiodusi98@gmail.com>
2022-07-17 23:07:00 -0400

    took address line away

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -38,16 +38,16 @@ function ContactForm() {
       <h1 data-testid="h1tag">Contact me</h1>
       <form id="contact-form" onSubmit={handleSubmit}>
         <div>
-          <label htmlFor="name">Name:</label>
+          <label htmlFor="name">Name: </label>
           <input type="text" name="name" defaultValue={name} onBlur={handleChange} />
         </div>
         <div>
-          <label htmlFor="email">Email address:</label>
+          <label htmlFor="email">Email: </label>
           <input type="email" name="email" defaultValue={email} onBlur={handleChange} />
         </div>
         <div>
           <label htmlFor="message">Message:</label>
-          <textarea name="message" rows="5" defaultValue={message} onBlur={handleChange} />
+          <textarea name="message" rows="4" defaultValue={message} onBlur={handleChange} />
         </div>
         {errorMessage && (
           <div>
